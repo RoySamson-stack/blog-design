66fa3c0291aefdcd7c996cc5efdb123e0780cf0d
RoySamson-stack <you@example.com>
2024-07-16 21:43:34 +0300

    default project files

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <header>
+      <h1>IMPRESSION</h1>
+      <h1>MENU</h1>
+     </header>
+    <main>
+      <div>
+        MORE POSITIVE
+      </div>
+      <div>
+        <img src="#"/>
+      </div>
+      <div>
+
+      </div>
+    </main>
+    </div>
+  );
+}
+
+export default App;
